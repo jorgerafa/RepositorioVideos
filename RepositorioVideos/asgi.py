c0ba9f47d0480c7c47c5acc0c00168db534f7888
jorgerafa <perezmontesjorgerafael@gmail.com>
2023-11-25 13:20:23 -0300

    first commit

---
+++ RepositorioVideos/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for RepositorioVideos project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RepositorioVideos.settings")
+
+application = get_asgi_application()
